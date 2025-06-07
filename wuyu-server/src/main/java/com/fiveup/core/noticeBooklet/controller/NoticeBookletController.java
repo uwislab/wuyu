a76334b2839dac0022ef4d8ea1a1a9bc7aeb511e
huangxingcan <2542049378@qq.com>
2025-06-07 16:44:57 +0800

    feat: 通知册相关功能初始

---
+++ wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/controller/NoticeBookletController.java
@@ -0,0 +1,18 @@
+package com.fiveup.core.noticeBooklet.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 通知册相关接口
+ *
+ * @author huangxingcan
+ * @date 2025/6/7 16:38
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/noticeBooklet")
+public class NoticeBookletController {
+
+}
