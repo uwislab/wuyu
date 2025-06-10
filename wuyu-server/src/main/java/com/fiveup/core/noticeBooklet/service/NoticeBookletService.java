978f0c11e536a787a0588c35d38b2c6eaa13c524
2022081063-张家宝 <2200785479@qq.com>
2025-06-10 19:57:39 +0800

    feature-06-zjb-批量导出word并压缩成zip文件完善完毕

--- wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/service/NoticeBookletService.java
+++ wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/service/NoticeBookletService.java
@@ -1,4 +1,11 @@
 package com.fiveup.core.noticeBooklet.service;
 
+import com.fiveup.core.noticeBooklet.mapper.ScoreMapper;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
 public interface NoticeBookletService {
+
+    public void exportMultipleWords(HttpServletResponse response, List<Long> ids);
 }
--- wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/service/impl/NoticeBookletServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.fiveup.core.noticeBooklet.service.impl;
-
-import com.fiveup.core.noticeBooklet.service.NoticeBookletService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class NoticeBookletServiceImpl implements NoticeBookletService {
-}
