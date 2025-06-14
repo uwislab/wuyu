64b22c574deebb9b73682696bf1b0daaf29067f6
1994173235@qq.com <1994173235@qq.com>
2025-06-14 15:30:32 +0800

    feat-通知册页面展示

---
+++ wuyu-front/src/views/notice/index.vue
@@ -0,0 +1,225 @@
+<!--
+ * @Author: hezeliangfj
+ * @Date: 2025-06-14 12:54:59
+ * @LastEditors: hezeliangfj
+ * @LastEditTime: 2025-06-14 15:12:54
+ * @version: 0.0.1
+ * @FilePath: \wuyu-front\src\views\notice\index.vue
+ * @Descripttion: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <div class="filter-container">
+      <div class="left-filters">
+        <el-select v-model="gradeId" placeholder="请选择年级">
+          <el-option
+            v-for="item in grades"
+            :key="item"
+            :label="`${item}年级`"
+            :value="item">
+          </el-option>
+        </el-select>
+        <el-select v-model="classId" placeholder="请选择班级" style="margin-left: 20px;">
+          <el-option
+            v-for="item in classes"
+            :key="item"
+            :label="`${item}班`"
+            :value="item">
+          </el-option>
+        </el-select>
+      </div>
+      <div class="right-actions">
+        <el-button type="primary" @click="dialogVisible=true">批量导出<i class="el-icon-tickets"></i></el-button>
+      </div>
+    </div>
+    <el-dialog :close-on-click-modal="false"
+      title="批量导出"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>是否导出{{ gradeId ? gradeId + '年级' : '' }}{{ classId ? classId + '班' : '' }}的所有学生的通知册信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleExportBatch">确 定</el-button>
+      </span>
+    </el-dialog>
+    <el-table :data="paginatedList" style="width: 100%" id="dataTable">
+			<el-table-column fixed type="selection" tooltip-effect="dark">
+			</el-table-column>
+      <el-table-column prop="studentGrade" label="年级" >
+			</el-table-column>
+			<el-table-column prop="studentClassNumber" label="班级" >
+			</el-table-column>
+
+			<el-table-column prop="studentId" label="学生学号" >
+			</el-table-column>
+			<el-table-column prop="studentName" label="学生姓名">
+			</el-table-column>
+
+      <el-table-column label="操作" align="center" width="190px">
+        <template slot-scope="scope">
+          <el-button type="primary" @click="handleExport(scope.row)">导出<i class="el-icon-tickets"></i></el-button>
+        </template>
+      </el-table-column>
+		</el-table>
+    <!-- 分页器 -->
+		<div class="mt text_center">
+			<el-pagination :current-page="query.page" :page-sizes="[10, 30, 100]" :page-size="query.pageSize"
+				:total="totalCount" @size-change="handleSizeChange" @current-change="handleCurrentChange"
+				layout="total, sizes, prev, pager, next, jumper">
+			</el-pagination>
+		</div>
+		<!-- /分页器 -->
+  </div>
+</template>
+
+<script>
+import { getStudent, exportZip,noticeBooklet } from '@/api/notice.js'
+// import { create } from 'sortablejs';
+export default {
+  // name: 'NoticeIndex',
+  // computed: {
+  //   ...mapGetters([
+  //     'permission_routes'
+  //   ]),
+  //   menuRoutes() {
+  //     return this.permission_routes
+  //   }
+  // },
+  data() {
+    return {
+      dialogVisible: false,
+      list: [],
+      grades: [],
+      classNames: [],
+      classes: [],
+      gradeId: null,
+      classId: null,
+      // 查询
+			query: {
+				"pageSize": 10,
+				"page": 1,
+				"courseName": "",
+				"courseType": "",
+				"teacherName": "",
+				"studentNum": "",
+				"studentName": "",
+				"login_time": "",
+				"create_time": "",
+				"orderby": `create_time desc`
+			},
+      // 数据
+			total: 0,//数据总数
+			list: [],
+			list_user_course_teacher: [],
+			deleteIds: [],
+    }
+  },
+  computed: {
+    filteredClasses() {
+      if (!this.gradeId) return []
+      const uniqueClasses = new Set()
+      this.classNames.forEach(item => {
+        if (item[0] === this.gradeId) {
+          uniqueClasses.add(item[1])
+        }
+      })
+      return Array.from(uniqueClasses).sort((a, b) => a - b)
+    },
+    // 过滤后的数据
+    filteredList() {
+      let result = [...this.list]
+
+      // 根据年级和班级筛选
+      if (this.gradeId) {
+        result = result.filter(item => item.studentGrade === this.gradeId)
+      }
+      if (this.classId) {
+        result = result.filter(item => item.studentClassNumber === this.classId)
+      }
+
+      return result
+    },
+    // 分页后的数据
+    paginatedList() {
+      const start = (this.query.page - 1) * this.query.pageSize
+      const end = start + this.query.pageSize
+      return this.filteredList.slice(start, end)
+    },
+    // 总数据量
+    totalCount() {
+      return this.filteredList.length
+    }
+  },
+  watch: {
+    gradeId: {
+      handler(newVal) {
+        this.classId = null
+        this.classes = this.filteredClasses
+        this.query.page = 1 // 重置页码
+      },
+      immediate: true
+    },
+    classId: {
+      handler() {
+        this.query.page = 1 // 重置页码
+      }
+    }
+  },
+  methods: {
+    async fetchCourse() {
+      const params = {}
+      const res = await getStudent(params)
+      this.grades = res.data.grades
+      this.classNames = res.data.classNames
+    },
+    async fetchNoticeBooklet() {
+      const params = {
+        isRemark: false
+      }
+      const res = await noticeBooklet({params})
+      this.list = res.data
+    },
+    handleSizeChange(pageSize) {
+      this.query.pageSize = pageSize
+      this.query.page = 1 // 切换每页条数时重置为第一页
+    },
+    handleCurrentChange(page) {
+      this.query.page = page
+    },
+    handleExport (row) {
+      console.log('222',row)
+    },
+    handleExportBatch() {
+      // Implementation of handleExportBatch method
+    },
+    handleClose() {
+      // Implementation of handleClose method
+    }
+  },
+  created() {
+    this.fetchCourse()
+    this.fetchNoticeBooklet()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.filter-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  // margin-bottom: 20px;
+
+  .left-filters {
+  margin-top: 20px;
+    display: flex;
+    // gap: 10px;
+    margin-left: 20px;
+  }
+
+  .right-actions {
+    margin-right: 20px;
+  }
+}
+</style>
