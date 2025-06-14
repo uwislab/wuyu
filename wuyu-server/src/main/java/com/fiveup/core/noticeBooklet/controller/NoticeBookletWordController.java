e68871585b91eb1771072898ce5ffdddb53323d3
李守懿 <worang_2022@qq.com>
2025-06-14 21:09:18 +0800

    Revert "通知册展示"
    This reverts commit 3c5816de872f776ebbca1ce2fe79fffb6dcaa242.

--- wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/controller/NoticeBookletWordController.java
+++ wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/controller/NoticeBookletWordController.java
@@ -2,8 +2,6 @@
 package com.fiveup.core.noticeBooklet.controller;
 
 import cn.hutool.http.HttpResponse;
-import com.fiveup.core.common.exception.ApiException;
-import com.fiveup.core.common.exception.GlobalExceptionHandler;
 import com.fiveup.core.management.common.enums.BizErrorCodeEnum;
 import com.fiveup.core.noticeBooklet.domain.NoticeBooklet;
 import com.fiveup.core.noticeBooklet.service.NoticeBookletService;
@@ -28,18 +26,22 @@ public class NoticeBookletWordController {
     private NoticeBookletWordService noticeBookletWordService;
 
     /**
-     * 根据学生ID生成单个Word文件
+     * 根据学生信息生成Word文件
      * @param studentId 学号
+     * @param classId 班级id
+     * @param gradeId 年级id
      * @return 响应结果
      */
-    @PostMapping("/generate")
-    public void generateWord(@RequestParam Integer studentId, HttpServletResponse response){
+    @GetMapping("/generate")
+    public void generateWord(@RequestParam(required = false) Integer studentId, @RequestParam(required = false) Integer classId, @RequestParam(required = false) Integer gradeId, HttpServletResponse response){
         try {
             // 查询通知册内容
-            List<NoticeBooklet> noticeBooklets = noticeBookletService.getNoticeBooklet(studentId, null, null,true);
+            List<NoticeBooklet> noticeBooklets = noticeBookletService.getNoticeBooklet(studentId, classId, gradeId,true);
             if (noticeBooklets != null && !noticeBooklets.isEmpty()) {
+                for (NoticeBooklet noticeBooklet : noticeBooklets) {
                     // 生成Word文件
-                    noticeBookletWordService.generateWord(noticeBooklets.get(0),response);
+                    noticeBookletWordService.generateWord(noticeBooklet,response);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
