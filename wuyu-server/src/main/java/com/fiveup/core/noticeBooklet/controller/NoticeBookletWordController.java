ec79a5722308c368e006246e9765595693fc89c4
DoNotBeLoser <3203166070@qq.com>
2025-06-14 18:38:01 +0800

    根据通知册内容生成word(最终版)

--- wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/controller/NoticeBookletWordController.java
+++ wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/controller/NoticeBookletWordController.java
@@ -1,14 +1,17 @@
 // NoticeBookletWordController.java
 package com.fiveup.core.noticeBooklet.controller;
 
+import cn.hutool.http.HttpResponse;
 import com.fiveup.core.management.common.enums.BizErrorCodeEnum;
 import com.fiveup.core.noticeBooklet.domain.NoticeBooklet;
 import com.fiveup.core.noticeBooklet.service.NoticeBookletService;
 import com.fiveup.core.noticeBooklet.service.NoticeBookletWordService;
 import com.fiveup.core.management.common.CommonResponse;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -30,21 +33,29 @@ public class NoticeBookletWordController {
      * @return 响应结果
      */
     @GetMapping("/generate")
-    public CommonResponse<String> generateWord(@RequestParam(required = false) Integer studentId, @RequestParam(required = false) Integer classId, @RequestParam(required = false) Integer gradeId) {
+    public void generateWord(@RequestParam(required = false) Integer studentId, @RequestParam(required = false) Integer classId, @RequestParam(required = false) Integer gradeId, HttpServletResponse response){
         try {
             // 查询通知册内容
-            List<NoticeBooklet> noticeBooklets = noticeBookletService.getNoticeBooklet(studentId, classId, gradeId,false);
+            List<NoticeBooklet> noticeBooklets = noticeBookletService.getNoticeBooklet(studentId, classId, gradeId,true);
             if (noticeBooklets != null && !noticeBooklets.isEmpty()) {
                 for (NoticeBooklet noticeBooklet : noticeBooklets) {
                     // 生成Word文件
-                    noticeBookletWordService.generateWord(noticeBooklet);
+                    noticeBookletWordService.generateWord(noticeBooklet,response);
                 }
-                return CommonResponse.ok("Word文件生成成功");
-            } else {
-                return CommonResponse.fail(BizErrorCodeEnum.valueOf("未查询到通知册内容"));
             }
         } catch (IOException e) {
-            return CommonResponse.fail(BizErrorCodeEnum.valueOf("Word文件生成失败：" + e.getMessage()));
+            throw new RuntimeException(e.getMessage());
         }
     }
+
+    @GetMapping("/generateHTML")
+    public CommonResponse<String> generateHTML(@RequestParam Integer studentId) throws IOException {
+            // 查询通知册内容
+            List<NoticeBooklet> noticeBooklets = noticeBookletService.getNoticeBooklet(studentId, null, null,true);
+            if(noticeBooklets.isEmpty()) {
+                return CommonResponse.fail(500, "未找到通知册内容");
+            }
+            return CommonResponse.ok(noticeBookletWordService.generateWordToHTML(noticeBooklets.get(0)));
+    }
+
 }
