0f0c7db2a7d104decfb3f6536ec717af569a0b22
DoNotBeLoser <3203166070@qq.com>
2025-06-14 14:53:20 +0800

    根据通知册内容生成word

---
+++ wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/controller/NoticeBookletWordController.java
@@ -0,0 +1,50 @@
+// NoticeBookletWordController.java
+package com.fiveup.core.noticeBooklet.controller;
+
+import com.fiveup.core.management.common.enums.BizErrorCodeEnum;
+import com.fiveup.core.noticeBooklet.domain.NoticeBooklet;
+import com.fiveup.core.noticeBooklet.service.NoticeBookletService;
+import com.fiveup.core.noticeBooklet.service.NoticeBookletWordService;
+import com.fiveup.core.management.common.CommonResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/noticeBooklet/word")
+public class NoticeBookletWordController {
+
+    @Autowired
+    private NoticeBookletService noticeBookletService;
+
+    @Autowired
+    private NoticeBookletWordService noticeBookletWordService;
+
+    /**
+     * 根据学生信息生成Word文件
+     * @param studentId 学号
+     * @param classId 班级id
+     * @param gradeId 年级id
+     * @return 响应结果
+     */
+    @GetMapping("/generate")
+    public CommonResponse<String> generateWord(@RequestParam(required = false) Integer studentId, @RequestParam(required = false) Integer classId, @RequestParam(required = false) Integer gradeId) {
+        try {
+            // 查询通知册内容
+            List<NoticeBooklet> noticeBooklets = noticeBookletService.getNoticeBooklet(studentId, classId, gradeId);
+            if (noticeBooklets != null && !noticeBooklets.isEmpty()) {
+                for (NoticeBooklet noticeBooklet : noticeBooklets) {
+                    // 生成Word文件
+                    noticeBookletWordService.generateWord(noticeBooklet);
+                }
+                return CommonResponse.ok("Word文件生成成功");
+            } else {
+                return CommonResponse.fail(BizErrorCodeEnum.valueOf("未查询到通知册内容"));
+            }
+        } catch (IOException e) {
+            return CommonResponse.fail(BizErrorCodeEnum.valueOf("Word文件生成失败：" + e.getMessage()));
+        }
+    }
+}
--- wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/utils/WordUtils.java
+++ wuyu-server/src/main/java/com/fiveup/core/noticeBooklet/utils/WordUtils.java
@@ -1,3 +1,4 @@
+
 package com.fiveup.core.noticeBooklet.utils;
 
 import cn.afterturn.easypoi.word.WordExportUtil;
