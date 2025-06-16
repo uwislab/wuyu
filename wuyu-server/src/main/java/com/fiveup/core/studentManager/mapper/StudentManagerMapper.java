30b47fad05e2ec5195f9ce384cb3eafcb16ce0bb
M07罗枧玮 <161919912+hqldsb@users.noreply.github.com>
2025-06-16 19:55:54 +0800

    功能完成，修复一些bug

--- wuyu-server/src/main/java/com/fiveup/core/studentManager/mapper/StudentManagerMapper.java
+++ wuyu-server/src/main/java/com/fiveup/core/studentManager/mapper/StudentManagerMapper.java
@@ -2,13 +2,12 @@ package com.fiveup.core.studentManager.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.fiveup.core.management.model.School;
-import com.fiveup.core.questionnaire.entity.Class;
 import com.fiveup.core.studentManager.entity.StudentManager;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import com.fiveup.core.studentManager.pojo.StudentManagerQuery;
 import com.fiveup.core.studentManager.pojo.StudentVO;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -21,6 +20,15 @@ public interface StudentManagerMapper extends BaseMapper<StudentManager> {
     @Select("select concat(grade,'#',class) from basic_class where deleted=0 group by class,grade;")
     Set<String> selectclassgrade();
 
+    //添加学生
+    @Insert("INSERT INTO basic_student (student_num,student_name,gender,class_id,grade_id,parent_phone_num,deleted,isreview,isenter,school_id)" +
+            "VALUES (#{studentNum},#{studentName},#{gender},#{classId},#{gradeId},#{parentPhoneNum},#{deleted},#{isreview},#{isenter},#{schoolId})")
+    void insertStudent(StudentManager studentManager);
+
+    //根据学校年级班级名查询class_id
+    @Select("select id from basic_class where grade_id=#{gradeId} and school_id=#{schoolId} and class_name=#{className}")
+    int selectClassId(int gradeId, int schoolId, String className);
+
     //分页查询学生
     List<StudentVO> getStudentPage(StudentManagerQuery studentManagerQuery);
 
@@ -29,7 +37,7 @@ public interface StudentManagerMapper extends BaseMapper<StudentManager> {
     List<School> getSchool();
 
     //获取班级列表
-    @Select("select DISTINCT class_name from basic_class order by class_name")
+    @Select("select DISTINCT class_name from basic_class where deleted = 0 order by class_name")
     List<String> getClassName();
 
     //获取年纪列表
