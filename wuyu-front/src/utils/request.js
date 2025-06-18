0f61c6509c25f3ca095ac9f289631b99d4f8f8e3
1994173235@qq.com <1994173235@qq.com>
2025-06-18 22:31:13 +0800

    fix-修改request

--- wuyu-front/src/utils/request.js
+++ wuyu-front/src/utils/request.js
@@ -2,7 +2,7 @@
  * @Author: hezeliangfj
  * @Date: 2025-06-13 09:30:51
  * @LastEditors: hezeliangfj
- * @LastEditTime: 2025-06-17 19:15:14
+ * @LastEditTime: 2025-06-18 22:02:01
  * @version: 0.0.1
  * @FilePath: \wuyu-front\src\utils\request.js
  * @Descripttion: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
