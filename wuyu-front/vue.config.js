b64c75e9fb689b14747892bb8a285d0c4ccd653c
M04李顺 <161947809+3256395073@users.noreply.github.com>
2025-06-25 20:24:03 +0800

    端口修改

--- wuyu-front/vue.config.js
+++ wuyu-front/vue.config.js
@@ -34,10 +34,10 @@ module.exports = {
     proxy: {
       "/api": {
         // target: "http://58.87.88.61:9200", // 后台端口号
-         target: "http://localhost:9080",
+         // target: "http://localhost:9080",
         //target: "http://36.111.68.174:33380",
         //target: "http://36.111.68.174:33380",
-        // target: "http://us.uwis.cn:9080",
+        target: "http://us.uwis.cn:9080",
         // target: "http://localhost:33381",
         changeOrigin: true,
         //ws: true,
