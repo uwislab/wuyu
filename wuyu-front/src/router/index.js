2a169e58ee423da07d696f88be6bbcc11969f009
Brian <3256395073@qq.com>
2025-06-25 20:06:02 +0800

    fix:对被其他组提交覆盖功能的修改

--- wuyu-front/src/router/index.js
+++ wuyu-front/src/router/index.js
@@ -654,6 +654,12 @@ export const asyncRoutes = [
         meta: { title: "数据展示", role: [0, 1, 2, 3] },
       },
       {
+        path: "charts",
+        component: () => import("@/views/course_score/charts"),
+        name: "成绩图表",
+        meta: { title: "成绩图表", role: [0, 1, 2,4] },
+      },
+      {
         path: "view",
         component: () => import("@/views/course_score/view"),
         name: "成绩录入",
